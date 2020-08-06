451c40a584e34d6fd4563770d6ef355f44e78e9e
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-06 12:50:11 -0400

    added server get status

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "Its deployed on heroku, and works!" });
+});
 
 module.exports = server;
